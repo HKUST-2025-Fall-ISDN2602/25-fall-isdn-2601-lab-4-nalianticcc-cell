5b02090b4f65a9c1960fdff0697e11d741584869
FENGshijie <nalianticcc@gmail.com>
2025-12-02 10:35:28 +0800

    loaded

--- Lab4_T2_Skeleton/Lab4_T2_Skeleton.ino
+++ Lab4_T2_Skeleton/Lab4_T2_Skeleton.ino
@@ -1,9 +1,9 @@
 /*Change all the ? in the code to Control the direction of rotation based on whether your selected pin is touched.*/
 
-#define IN1   ？  // Replace the ? with the GPIO pin you selected to connect IN1
-#define IN2   ？   // Replace the ? with the GPIO pin you selected to connect IN2
-#define A  ？ // Replace the ? with the GPIO pin you selected to connect encoder A
-#define B  ？ // Replace the ? with the GPIO pin you selected to connect encoder B
+#define IN1   32  // Replace the ? with the GPIO pin you selected to connect IN1
+#define IN2   22   // Replace the ? with the GPIO pin you selected to connect IN2
+#define A  18 // Replace the ? with the GPIO pin you selected to connect encoder A
+#define B  35 // Replace the ? with the GPIO pin you selected to connect encoder B
 
 
 int A_data;
@@ -12,13 +12,13 @@ int B_data;
 String command;
 
 void setup() {
-  Serial.begin(？); //Define baud
+  Serial.begin(115200); //Define baud
 
-  pinMode(？, ？);  // Set IN1 as an output pin
-  pinMode(？, ？);  // Set IN2 as an output pin
+  pinMode( IN1, OUTPUT);  // Set IN1 as an output pin
+  pinMode(IN2, OUTPUT);  // Set IN2 as an output pin
 
-  pinMode(？, ？);  // Set A as an input pin
-  pinMode(？, ？);  // Set B as an input pin
+  pinMode(A, INPUT);  // Set A as an input pin
+  pinMode(B, INPUT);  // Set B as an input pin
 
 }
 
@@ -29,19 +29,19 @@ void loop() {
         command.trim(); // Remove any leading or trailing whitespace
         if (command == "C") {
             // clockwise rotation
-            digitalWrite(？, ？);
-            digitalWrite(？, ？; 
+            digitalWrite(IN1, HIGH);
+            digitalWrite(IN2, LOW); 
 
         } 
         else if (command == "A") { 
             // anticlockwise rotation
-            digitalWrite(?, ?);
-            digitalWrite(?, ?); 
+            digitalWrite(IN1, LOW);
+            digitalWrite(IN2, HIGH); 
         }
         }
     // Read values of A and B using digitalRead(Pin)
-    A_data=digitalRead(?);
-    B_data=digitalRead(?);
+    A_data=digitalRead(A);
+    B_data=digitalRead(B);
     // Plot A B in Serial Plotter
     Serial.print("A:");
     Serial.print(A_data);
@@ -49,4 +49,5 @@ void loop() {
     Serial.print("B:");
     Serial.print(B_data);
     Serial.println("\t"); 
+}
 }
